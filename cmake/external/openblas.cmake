ecb86135d0df0a4ff0b2f62ad12ff4b35e3e6927
liaogang <liaogang@baidu.com>
2017-01-12 20:04:27 +0800

    update

--- cmake/external/openblas.cmake
+++ cmake/external/openblas.cmake
@@ -32,6 +32,8 @@ IF(NOT ${CBLAS_FOUND})
         LIST(APPEND CBLAS_LIBRARIES gfortran pthread)
     ENDIF(CMAKE_COMPILER_IS_GNUCC)
 
+    message("######### ${CMAKE_Fortran_COMPILER}")
+
     ExternalProject_Add(
         openblas
         GIT_REPOSITORY      "git://github.com/xianyi/OpenBLAS.git"
@@ -40,7 +42,7 @@ IF(NOT ${CBLAS_FOUND})
         INSTALL_DIR         ${CBLAS_INSTALL_DIR}
         BUILD_IN_SOURCE     1
         CONFIGURE_COMMAND   ""
-        BUILD_COMMAND       make CC=${CMAKE_C_COMPILER} FC=${CMAKE_Fortran_COMPILER} NO_SHARED=1 libs netlib
+        BUILD_COMMAND       make CC=${CMAKE_C_COMPILER} FC=${CMAKE_Fortran_COMPILER} NO_SHARED=1 # libs netlib
         INSTALL_COMMAND     make install NO_SHARED=1 PREFIX=<INSTALL_DIR>
         UPDATE_COMMAND      ""
     )
