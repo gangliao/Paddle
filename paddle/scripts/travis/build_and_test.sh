e7f58d2b2fd03f5a26b9bdd96784a3cc5747288a
liaogang <liaogang@baidu.com>
2017-01-12 19:31:02 +0800

    update

--- paddle/scripts/travis/build_and_test.sh
+++ paddle/scripts/travis/build_and_test.sh
@@ -3,12 +3,10 @@ source ./common.sh
 
 NPROC=1
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  which gfortran-4.8
-  ll /opt/
   export PYTHONPATH=/opt/python/2.7.12/lib/python2.7/site-packages
   export PYTHONHOME=/opt/python/2.7.12
   export PATH=/opt/python/2.7.12/bin:${PATH}
-  cmake .. -DCMAKE_CXX_COMPILER=/usr/bin/g++ -DON_TRAVIS=ON -DON_COVERALLS=ON -DCOVERALLS_UPLOAD=ON ${EXTRA_CMAKE_OPTS}
+  cmake .. -DCMAKE_Fortran_COMPILER=/usr/bin/gfortran-4.8 -DON_TRAVIS=ON -DON_COVERALLS=ON -DCOVERALLS_UPLOAD=ON ${EXTRA_CMAKE_OPTS}
   NRPOC=`nproc`
   make -j $NPROC
   make coveralls
