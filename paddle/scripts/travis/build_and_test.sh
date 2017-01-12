cc667a82a31f3bc39a3b3d5a0ce0d314c169105b
liaogang <liaogang@baidu.com>
2017-01-12 20:54:52 +0800

    update

--- paddle/scripts/travis/build_and_test.sh
+++ paddle/scripts/travis/build_and_test.sh
@@ -3,6 +3,7 @@ source ./common.sh
 
 NPROC=1
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  /usr/bin/gfortran-4.8 --version
   export PYTHONPATH=/opt/python/2.7.12/lib/python2.7/site-packages
   export PYTHONHOME=/opt/python/2.7.12
   export PATH=/opt/python/2.7.12/bin:${PATH}
