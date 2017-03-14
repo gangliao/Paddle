e0172a575ca8b97c3b3cae7543d8cd184d7041b0
liaogang <liaogang@baidu.com>
2017-03-14 14:43:58 +0800

    update clapack

--- cmake/external/clapack.cmake
+++ cmake/external/clapack.cmake
@@ -12,55 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ENABLE_LANGUAGE(Fortran OPTIONAL)
 
-# IF (NOT CMAKE_Fortran_COMPILER) 
+INCLUDE(ExternalProject)
 
-    INCLUDE(ExternalProject)
+SET(CLAPACK_SOURCES_DIR ${THIRD_PARTY_PATH}/clapack)
+SET(CLAPACK_INSTALL_DIR ${THIRD_PARTY_PATH}/install/clapack)
+SET(CLAPACK_INCLUDE_DIR "${CLAPACK_INSTALL_DIR}/include" CACHE PATH "clapack include directory." FORCE)
 
-    SET(CLAPACK_SOURCES_DIR ${THIRD_PARTY_PATH}/clapack)
-    SET(CLAPACK_INSTALL_DIR ${THIRD_PARTY_PATH}/install/clapack)
-    SET(CLAPACK_INCLUDE_DIR "${CLAPACK_INSTALL_DIR}/include" CACHE PATH "clapack include directory." FORCE)
+SET(CLAPACK_LIBS_DIR "${CLAPACK_INSTALL_DIR}/lib")
+SET(CLAPACK_LIBRARIES
+    ${CLAPACK_LIBS_DIR}/liblapack.a
+    ${CLAPACK_LIBS_DIR}/libf2c.a
+    ${CLAPACK_LIBS_DIR}/libcblaswr.a
+    CACHE FILEPATH "CLAPACK_LIBRARIES" FORCE)
 
-    SET(CLAPACK_LIBS_DIR "${CLAPACK_INSTALL_DIR}/lib")
-    IF(WIN32)
-        set(CLAPACK_LIBRARIES "${CLAPACK_INSTALL_DIR}/lib/lapack.lib" CACHE FILEPATH "CLAPACK_LIBRARIES" FORCE)
-    ELSE(WIN32)
-        set(CLAPACK_LIBRARIES "${CLAPACK_INSTALL_DIR}/lib/liblapack.a" CACHE FILEPATH "CLAPACK_LIBRARIES" FORCE)
-    ENDIF(WIN32)
+INCLUDE_DIRECTORIES(${CLAPACK_INCLUDE_DIR})
 
-    INCLUDE_DIRECTORIES(${CLAPACK_INCLUDE_DIR})
+ExternalProject_Add(
+    clapack
+    ${EXTERNAL_PROJECT_LOG_ARGS}
+    GIT_REPOSITORY  "https://github.com/Xreki/clapack.git"
+#    GIT_TAG         3.2.1
+    PREFIX          ${CLAPACK_SOURCES_DIR}
+    UPDATE_COMMAND  ""
+    INSTALL_COMMAND ""
+    CMAKE_ARGS      -DCMAKE_INSTALL_PREFIX=${CLAPACK_INSTALL_DIR}
+    CMAKE_ARGS      -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+    CMAKE_ARGS      -DBUILD_SINGLE=OFF
+    CMAKE_ARGS      -DBUILD_COMPLEX=OFF
+    CMAKE_ARGS      -DBUILD_COMPLEX16=OFF
+    CMAKE_ARGS      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    CMAKE_ARGS      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+)
 
-    ExternalProject_Add(
-        clapack
-        ${EXTERNAL_PROJECT_LOG_ARGS}
-        GIT_REPOSITORY  "https://github.com/Xreki/clapack.git"
-        GIT_TAG         3.2.1
-        PREFIX          ${CLAPACK_SOURCES_DIR}
-        UPDATE_COMMAND  ""
-        INSTALL_COMMAND ""
-        CMAKE_ARGS      -DCMAKE_INSTALL_PREFIX=${CLAPACK_INSTALL_DIR}
-        CMAKE_ARGS      -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        CMAKE_ARGS      -DBUILD_SINGLE=OFF
-        CMAKE_ARGS      -DBUILD_COMPLEX=OFF
-        CMAKE_ARGS      -DBUILD_COMPLEX16=OFF
-        CMAKE_ARGS      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-        CMAKE_ARGS      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    )
+ExternalProject_Add_Step(
+    clapack clapack_install
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/blaswrap.h" "${CLAPACK_INCLUDE_DIR}/blaswrap.h"
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/clapack.h" "${CLAPACK_INCLUDE_DIR}/clapack.h"
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/f2c.h" "${CLAPACK_INCLUDE_DIR}/f2c.h"                
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/BLAS/WRAP/libcblaswr.a" "${CLAPACK_LIBS_DIR}/libcblaswr.a"
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/F2CLIBS/libf2c/libf2c.a" "${CLAPACK_LIBS_DIR}/libf2c.a"
+    COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/SRC/liblapack.a" "${CLAPACK_LIBS_DIR}/liblapack.a"
+    DEPENDEES install
+)
 
-message("#### ${CLAPACK_LIBS_DIR}")
-
-    ExternalProject_Add_Step(
-        clapack clapack_install
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/blaswrap.h" "${CLAPACK_INCLUDE_DIR}/include/blaswrap.h"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/clapack.h" "${CLAPACK_INCLUDE_DIR}/include/clapack.h"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack/INCLUDE/f2c.h" "${CLAPACK_INCLUDE_DIR}/include/f2c.h"                
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/BLAS/SRC/libblas.a" "${CLAPACK_LIBS_DIR}/libblas.a"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/F2CLIBS/libf2c/libf2c.a" "${CLAPACK_LIBS_DIR}/libf2c.a"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CLAPACK_SOURCES_DIR}/src/clapack-build/SRC/liblapack.a" "${CLAPACK_LIBS_DIR}/liblapack.a"
-        DEPENDEES install
-    )
-
-    LIST(APPEND external_project_dependencies clapack)
-
-# ENDIF(NOT CMAKE_Fortran_COMPILER)
+LIST(APPEND external_project_dependencies clapack)
--- cmake/external/openblas.cmake
+++ cmake/external/openblas.cmake
@@ -27,12 +27,6 @@ IF(NOT ${CBLAS_FOUND})
         SET(CBLAS_LIBRARIES "${CBLAS_INSTALL_DIR}/lib/libopenblas.a" CACHE FILEPATH "openblas library" FORCE)
     ENDIF(WIN32)
 
-    SET(CBLAS_COMPILER_ARGS "CC=${CMAKE_C_COMPILER} HOSTCC=${CMAKE_C_COMPILER}")
-
-    IF(CMAKE_Fortran_COMPILER)
-        SET(CBLAS_COMPILER_ARGS "FC=${CMAKE_Fortran_COMPILER} CC=${CMAKE_C_COMPILER} HOSTCC=${CMAKE_C_COMPILER}")
-        LIST(APPEND CBLAS_LIBRARIES gfortran pthread)
-    ENDIF(CMAKE_Fortran_COMPILER)
 
     ExternalProject_Add(
         openblas
@@ -42,20 +36,20 @@ IF(NOT ${CBLAS_FOUND})
         PREFIX              ${CBLAS_SOURCES_DIR}
         INSTALL_DIR         ${CBLAS_INSTALL_DIR}
         BUILD_IN_SOURCE     1
-        BUILD_COMMAND       ${CMAKE_MAKE_PROGRAM} ${CBLAS_COMPILER_ARGS} NO_SHARED=1 libs netlib
+        BUILD_COMMAND       ${CMAKE_MAKE_PROGRAM} CC=${CMAKE_C_COMPILER} HOSTCC=${CMAKE_C_COMPILER} NO_SHARED=1 libs netlib
         INSTALL_COMMAND     ${CMAKE_MAKE_PROGRAM} install NO_SHARED=1 PREFIX=<INSTALL_DIR>
         UPDATE_COMMAND      ""
         CONFIGURE_COMMAND   ""
     )
 
-    ExternalProject_Add_Step(
-        openblas lapacke_install
-        COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_mangling_with_flags.h" "${CBLAS_INSTALL_DIR}/include/lapacke_mangling.h"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke.h" "${CBLAS_INSTALL_DIR}/include/lapacke.h"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_config.h" "${CBLAS_INSTALL_DIR}/include/lapacke_config.h"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_utils.h" "${CBLAS_INSTALL_DIR}/include/lapacke_utils.h"
-        DEPENDEES install
-    )
+#     ExternalProject_Add_Step(
+#         openblas lapacke_install
+#         COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_mangling_with_flags.h" "${CBLAS_INSTALL_DIR}/include/lapacke_mangling.h"
+#  #       COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke.h" "${CBLAS_INSTALL_DIR}/include/lapacke.h"
+#         COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_config.h" "${CBLAS_INSTALL_DIR}/include/lapacke_config.h"
+#         COMMAND ${CMAKE_COMMAND} -E copy "${CBLAS_SOURCES_DIR}/src/openblas/lapack-netlib/LAPACKE/include/lapacke_utils.h" "${CBLAS_INSTALL_DIR}/include/lapacke_utils.h"
+#         DEPENDEES install
+#     )
 
     LIST(APPEND external_project_dependencies openblas)
 ENDIF(NOT ${CBLAS_FOUND})
